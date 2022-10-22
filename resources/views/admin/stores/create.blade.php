dbe5e72ae40c705de705ae5cf4db35f98ded6268
Agostinho Neto <agostneto6@gmail.com>
2022-10-22 10:16:20 -0300

    alteração 22/10

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -48,34 +48,49 @@
         
         <div class="form-group">
             <label>Nome Loja:</label>
-            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
         </div>
         @error('name')
             <div class="invalid-feedback">
                 {{$message}}
             </div>
         @enderror
-
         <div class="form-group">
             <label>Descrição:</label>
-            <input type="text" name="description" class="form-control"  />
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror " value="{{old('description')}}"/>
         </div>
-        
+        @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror  
         <div class="form-group">
             <label>Telefone:</label>
-            <input type="text" name="phone"  class="form-control" />
+            <input type="text" name="phone"  class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}"/>
         </div>
-
+        @error('phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror  
         <div class="form-group">
             <label>Celular:</label>
-            <input type="text" name="mobile_phone" class="form-control"  />
+            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror" value="{{old('mobile_phone')}}"  />
         </div>
-
+        @error('mobile_phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror  
         <div class="form-group">
             <label>slug:</label>
-            <input type="text" name="slug" class="form-control"  />
+            <input type="text" name="slug" class="form-control @error('slug') is-invalid @enderror"   value="{{old('slug')}}">
         </div>
-
+        @error('slug')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+         @enderror  
         <div class="form-group">
             <label>Usuário:</label>
             <select name="user" class="form-control">
